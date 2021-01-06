4a23b297bb6e8e55c5785893b1fe8dcb43001a7d
faustabmnyh <faustaabimanyu@gmail.com>
2021-01-06 07:39:32 +0700

    Fix responsive

--- e-commerce-v3/src/components/Header/index.js
+++ e-commerce-v3/src/components/Header/index.js
@@ -87,75 +87,130 @@ const Header = () => {
                   <button className="header__btn">Sign In</button>
                 </Link>
               ) : (
-                <div className="header__userProfileDropdown">
-                  <div className="header__userProfile">
-                    <Avatar
-                      className="header__photo"
-                      src={aboutUser.photo_profile}
-                    />
-                    <p>{aboutUser.username}</p>
-                  </div>
-                  <ul
-                    className={
-                      aboutUser && aboutUser.isAdmin && aboutUser.isSeller
-                        ? "header__dropdownContent admin-seller"
-                        : aboutUser && (aboutUser.isAdmin || aboutUser.isSeller)
-                        ? "header__dropdownContent admin"
-                        : "header__dropdownContent"
-                    }
-                  >
-                    <div>
-                      <li>
-                        <Link to="/profile">Profile</Link>
-                      </li>
-                      <li>
-                        <Link to="/orderhistory">Order History</Link>
-                      </li>
-                      <li>
-                        <Link to="/signin" onClick={handleSignout}>
-                          Sign Out
-                        </Link>
-                      </li>
+                <div className="header__profileDetails">
+                  <div className="header__cartBtnContainer responsive">
+                    <div
+                      className={
+                        cartItems.length === 0 ? "hide" : "header__cartNumber"
+                      }
+                    >
+                      {cartItems.length}
                     </div>
-                    {aboutUser && aboutUser.isAdmin && (
+                    <div className="header__cartBtn">
+                      <Link to="/cart">
+                        <LocalGroceryStoreIcon fontSize="small" />
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="header__userProfileDropdown">
+                    <div className="header__userProfileMobile">
+                      <Avatar
+                        className="header__photo"
+                        src={aboutUser.photo_profile}
+                      />
+                    </div>
+                    <div className="header__userProfile">
+                      <Avatar
+                        className="header__photo"
+                        src={aboutUser.photo_profile}
+                      />
+                      <p>{aboutUser.username}</p>
+                    </div>
+                    <ul
+                      className={
+                        aboutUser && aboutUser.isAdmin && aboutUser.isSeller
+                          ? "header__dropdownContent admin-seller"
+                          : aboutUser &&
+                            (aboutUser.isAdmin || aboutUser.isSeller)
+                          ? "header__dropdownContent admin"
+                          : "header__dropdownContent"
+                      }
+                    >
                       <div>
                         <li>
-                          <Link to="/dashboard">
-                            Dashboard <small>(admin)</small>
-                          </Link>
+                          <Link to="/profile">Profile</Link>
                         </li>
                         <li>
-                          <Link to="/productlists">
-                            Products <small>(admin)</small>
-                          </Link>
+                          <Link to="/orderhistory">Order History</Link>
+                        </li>
+                        {aboutUser && aboutUser.isSeller && (
+                          <>
+                            <li className="header__responsive">
+                              <Link to="/productlists/seller">
+                                Products <small>(seller)</small>
+                              </Link>
+                            </li>
+                            <li className="header__responsive">
+                              <Link to="/orderlist/seller">
+                                Orders <small>(seller)</small>
+                              </Link>
+                            </li>
+                          </>
+                        )}
+                        <li className="header__linksShop">
+                          <Link to="/shop">Shop</Link>
                         </li>
                         <li>
-                          <Link to="/orderlist">
-                            Orders <small>(admin)</small>
+                          <Link to="/signin" onClick={handleSignout}>
+                            Sign Out
                           </Link>
                         </li>
-                        <li>
-                          <Link to="/userslist">
-                            Users <small>(admin)</small>
-                          </Link>
-                        </li>
+                        <form
+                          className="header__cartInput responsive"
+                          onSubmit={handleSubmit}
+                        >
+                          <label htmlFor="search" className="header__searchBtn">
+                            <SearchIcon />
+                          </label>
+                          <input
+                            id="search"
+                            type="text"
+                            placeholder="Search..."
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                          />
+                        </form>
                       </div>
-                    )}
-                    {aboutUser && aboutUser.isSeller && (
-                      <div>
-                        <li>
-                          <Link to="/productlists/seller">
-                            Products <small>(seller)</small>
-                          </Link>
-                        </li>
-                        <li>
-                          <Link to="/orderlist/seller">
-                            Orders <small>(seller)</small>
-                          </Link>
-                        </li>
-                      </div>
-                    )}
-                  </ul>
+                      {aboutUser && aboutUser.isAdmin && (
+                        <div>
+                          <li>
+                            <Link to="/dashboard">
+                              Dashboard <small>(admin)</small>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/productlists">
+                              Products <small>(admin)</small>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/orderlist">
+                              Orders <small>(admin)</small>
+                            </Link>
+                          </li>
+                          <li>
+                            <Link to="/userslist">
+                              Users <small>(admin)</small>
+                            </Link>
+                          </li>
+                        </div>
+                      )}
+                      {aboutUser && aboutUser.isSeller && (
+                        <div>
+                          <li className="header__navSeller">
+                            <Link to="/productlists/seller">
+                              Products <small>(seller)</small>
+                            </Link>
+                          </li>
+                          <li className="header__navSeller">
+                            <Link to="/orderlist/seller">
+                              Orders <small>(seller)</small>
+                            </Link>
+                          </li>
+                        </div>
+                      )}
+                    </ul>
+                  </div>
                 </div>
               )}
             </li>
